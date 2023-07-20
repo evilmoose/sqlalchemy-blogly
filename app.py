f95976dac050012b02ef23291c8dd86a8b25489c
Evilmoose <ronnielopez@gmail.com>
2023-07-20 00:09:59 -0700

    Fixed db errors

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:654321@localhost:5432/blogly"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ronaldlopez:123456@localhost:5432/blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ihaveasecret'
 
